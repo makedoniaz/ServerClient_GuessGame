fb003ace03348383f03722f046bb9d301ab9c08b
Садыг Джафар Эмиль <Sadyg_he61@itstep.edu.az>
2023-11-02 21:48:03 +0400

    Setup socket on server side

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,7 +1,25 @@
-﻿public class Program
+﻿using System.Net;
+using System.Net.Sockets;
+
+public class Program
 {
+    static string ip = "127.0.0.1";
+    static short port = 8000;
+
     private static void Main()
     {
-        
+        var socket = new Socket(
+            addressFamily: AddressFamily.InterNetwork,
+            socketType: SocketType.Stream,
+            protocolType: ProtocolType.Tcp
+        );
+
+        var adress = IPAddress.Parse(ip);
+        var endpoint = new IPEndPoint(adress, port);
+
+        socket.Bind(endpoint);
+        socket.Listen(backlog: 5);
+
+        socket.Accept();
     }
 }
