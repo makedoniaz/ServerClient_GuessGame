e1dcf8948dc2a9947b7c220ae9457d5b3579fdf2
makedoniaz <jafar.sadig04@gmail.com>
2023-11-08 13:29:08 +0400

    Add TcpServer class

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Server.Services;
+using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -6,32 +7,24 @@ using System.Text;
 public class Program
 {
     static string ip = "127.0.0.1";
-    static short port = 8000;
+    static ushort port = 8000;
 
     private static void Main()
     {
+        var tcpServer = new TcpServer(ip, port);
+
         var randNumGenerator = new Random();
         int numToGuess = randNumGenerator.Next(0, 101);
 
-        var socket = new Socket(
-            addressFamily: AddressFamily.InterNetwork,
-            socketType: SocketType.Stream,
-            protocolType: ProtocolType.Tcp
-        );
-
-        var adress = IPAddress.Parse(ip);
-        var endpoint = new IPEndPoint(adress, port);
-
-
-        socket.Bind(endpoint);
-        socket.Listen(backlog: 5);
+        tcpServer.OpenConnection();
         Console.WriteLine($"Server started on {ip}:{port}");
 
-        var clientSocket = socket.Accept();
+        var clientSocket = tcpServer.AcceptClient();
         Console.WriteLine($"{clientSocket.RemoteEndPoint} connected");
         Console.WriteLine($"Number to guess: {numToGuess}");
 
-        clientSocket.Send(Encoding.UTF8.GetBytes("I came up with a number between 0 and 100.\nYour goal to guess this number withing 5 tries...\n"));
+        var rules = "I came up with a number between 0 and 100.\nYour goal to guess this number withing 5 tries...\n";
+        tcpServer.SendMessage(clientSocket, rules);
 
         while (true)
         {
---
+++ Server/Services/TcpServer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Services;
+
+public class TcpServer
+{
+    private Socket _socket;
+    private IPEndPoint _endpoint;
+
+    public EndPoint EndPoint => _endpoint;
+
+    public TcpServer(string ip, ushort port)
+    {
+        _socket = new Socket(
+            addressFamily: AddressFamily.InterNetwork,
+            socketType: SocketType.Stream,
+            protocolType: ProtocolType.Tcp
+        );
+
+        var adress = IPAddress.Parse(ip);
+        _endpoint = new IPEndPoint(adress, port);
+    }
+
+    public void OpenConnection(int backlog = 5)
+    {
+        _socket.Bind(_endpoint);
+        _socket.Listen(backlog);
+    }
+
+    public Socket AcceptClient() => _socket.Accept();
+
+    public void SendMessage(Socket receiver, string message) => receiver.Send(Encoding.UTF8.GetBytes(message));
+
+    public void SendMessageAsync(Socket receiver, string message) => receiver.SendAsync(Encoding.UTF8.GetBytes(message));
+}
